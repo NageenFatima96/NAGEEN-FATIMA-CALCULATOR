1b5bc20b7ad653b2c3de684f7a586e3b2207d2d1
NageenFatima96 <fnageen2@gmail.com>
2024-09-10 17:46:54 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,46 @@
+import streamlit as st
+st.write('Nageen')
+# from add import add_numbers
+# from subtract import subtract_numbers
+# from multiply import multiply_numbers
+# from divide import divide_numbers
+#
+# def calculator():
+#     while True:
+#         # Input two numbers and the operator
+#         num1 = float(input("Enter the first number: "))
+#         num2 = float(input("Enter the second number: "))
+#         operator = input("Enter the operator (+, -, *, /): ")
+#
+#         # Perform the calculation based on the operator
+#         if operator == '+':
+#             result = add_numbers(num1, num2)
+#         elif operator == '-':
+#             result = subtract_numbers(num1, num2)
+#         elif operator == '*':
+#             result = multiply_numbers(num1, num2)
+#         elif operator == '/':
+#             try:
+#                 result = divide_numbers(num1, num2)
+#             except ValueError as e:
+#                 print(f"Error: {e}")
+#                 continue
+#         else:
+#             print("Invalid operator!")
+#             continue
+#
+#         # Display the result
+#         print(f"Result: {result}")
+#
+#         # Ask to quit or continue
+#         choice = input("Do you want to quit? (Y/N): ").strip().lower()
+#         if choice == 'y':
+#             break
+#         elif choice == 'n':
+#             continue
+#         else:
+#             print("Invalid choice. Continuing by default.")
+#
+# if __name__ == "__main__":
+#     calculator()
+
